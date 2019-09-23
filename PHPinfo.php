222482f6ad22650f91c5f70673095dd74cab96b4
ThomasSueur <50668157+ThomasSueur@users.noreply.github.com>
2019-09-23 09:03:18 +0200

    Modification PHPinfo -> Bonjour Robert.
    Modification du fichier PHPinfo pour qu'il dise bonjour à Robert.

--- PHPinfo.php
+++ PHPinfo.php
@@ -1,10 +1,13 @@
 <html>
-<head>
-<title>phpinfo.php</title>
-</head>
-<body>
-<?php
-phpinfo();
-?>
-</body>
+	<head>
+		<title>phpinfo.php</title>
+	</head>
+
+	<body>
+	<?php
+		$nom = "Robert"; 
+		echo "<h1>TP PHP</h1>\n"; 
+		echo "<p>Bonjour $nom !<br />...</p>\n"; 
+	 ?>
+	</body>
 </html>
